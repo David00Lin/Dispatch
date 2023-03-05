e11ee9b105de31f745677a0e2050633b9cbc5cbc
David_Lin <yuhung00lin@gmail.com>
2023-03-05 14:38:04 +0900

    created index

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get("/", function (req, res) {
     console.log("Hello I'm on the start page here");
-    res.render('index');
+    res.render("index");
 })
 
 module.exports = router;
